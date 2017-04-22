ab47e895065f396c0d7fbc6e4050590feb229766
Mustafa Özpolat <m_ozpolat@msn.com>
2017-04-22 19:49:30 +0300

    Add project files.

---
+++ DenemeUygulaması/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DenemeUygulaması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HeloWorld");
+            Console.ReadLine();
+        }
+    }
+}
